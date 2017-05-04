bf073ef7a7bf91a10cd5e7a7ea8cf48fde2f4fca
smarin9 <smarin9@lsu.edu>
2017-05-03 21:31:33 -0500

    Correct Upload

--- prog3/sysbar.sh
+++ prog3/sysbar.sh
@@ -1,66 +1,85 @@
 #! /bin/bash
-echo hello world
-if [ "$1" = "help" ]
-        then
-        echo "This script accepts 2 parameters - Intervals in Seconds and Number of Collections"
-        echo "This script will automatically collect system usage information (CPU and disk)"
-        echo "for a number of interations, and generate two bargraph figures to report the system usage information"
-        echo "This script will allow no more than two parameters, and the input numbers should be in a reasonable range"
-elif [ $# -ne 2 ]
-        then
-        echo "Invalid Number of Arguments - 2 Arguments Needed"
-        echo
-else
-        args1=$1
-        args2=$2
-        echo "args1 = " $args1
-        echo "args2 = " $args2
-fi
 
-#Sets the date
-date +%H:%M:%S
-#mpstat $args1 $args2
-#iostat $args1 $args2
+#Name:		Marino, Sean
+#Project:   PA-3 (Shell Programming)
+#File:		sysbar.sh
+#Intructor: Feng Chen
+#Class:		cs4103-sp17
+#LogonID:   cs410363
+
+parameters()
+{
+	#Help command for using the script
+	#Basic check for accepting incorrect input
+	if [ "$1" = "help" ]
+	        then
+	        echo "This script accepts 2 parameters - Intervals in Seconds and Number of Collections"
+	        echo "This script will automatically collect system usage information (CPU and disk)"
+	        echo "for a number of interations, and generate two bargraph figures to report the system usage information"
+	        echo "This script will allow no more than two parameters, and the input numbers should be in a reasonable range"
+	elif [ $# -ne 2 ]
+	        then
+	        echo "Invalid Number of Arguments - 2 Arguments Needed"
+	        echo
+	else
+	        args1=$1
+	        args2=$2
+	fi
+}
+
+#Creates the .plot files for CPU Usage and Disk Usage
+
+makeFiles()
+{
+	printf "=stacked; usr; sys; idle\n" >> cpu_usage.plot
+	printf "title=CPU Usage Plot\n" >> cpu_usage.plot
+	printf "colors=yellow,red,black\n" >> cpu_usage.plot
+	printf "xlabel=Time\n" >> cpu_usage.plot
+	printf "ylabel=Percentage(%%)\n" >> cpu_usage.plot
+	printf "=table\n" >> cpu_usage.plot
+	printf "=norotate\n" >> cpu_usage.plot
+	printf "yformat=%%g\n" >> cpu_usage.plot
+
+	printf "=stacked; kB_read/s; kB_wrtn/s\n" >> disk_usage.plot
+	printf "title=DISK Usage Plot\n" >> disk_usage.plot
+	printf "colors=yellow,red\n" >> disk_usage.plot
+	printf "xlabel=Time\n" >> disk_usage.plot
+	printf "ylabel=Bandwith (kB/sec)\n" >> disk_usage.plot
+	printf "=table\n" >> disk_usage.plot
+	printf "=norotate\n" >> disk_usage.plot
+	printf "yformat=%%g\n" >> disk_usage.plot
+}
 
 #mpstat needs to retrieve usr, sys, idle 
 #iostat needs to retrieve kB-read/s, and kB_wrtn/s
-plotsetup()
-{
-	echo =stacked; A; B >> disk_usage.plot
-	echo title=This is an illustration title >> disk_usage.plot
-	echo colors=yellow,red >> disk_usage.plot
-	echo xlabel=This is X Label >> disk_usage.plot
-	echo ylabel=This is Y Label >> disk_usage.plot
-	echo =table >> disk_usage.plot
-	echo =norotate >> disk_usage.plot
-	echo yformat=%g >> disk_usage.plot
+#Retrieves the specified data from each column in mpstat and iostat
 
-	echo =stacked; A; B >> cpu_usage.plot
-	echo title=This is an illustration title >> cpu_usage.plot
-	echo colors=yellow,red >> cpu_usage.plot
-	echo xlabel=This is X Label >> cpu_usage.plot
-	echo ylabel=This is Y Label >> cpu_usage.plot
-	echo =table >> cpu_usage.plot
-	echo =norotate >> cpu_usage.plot
-	echo yformat=%g >> cpu_usage.plot
+statPlot()
+{
+	for (( i=0; i<"$2"; i++ )); do 
+		mpinfoAll=`mpstat | awk '{ $2=""; $3=""; $5=""; $7=""; $8=""; $9=""; $10=""; $11=""; $12=""; print $0;}'`
+		#Trims the header and locates the specified item in the row of data from mpstat
+		mpinfo=`echo ${mpinfoAll:76:100}`
+		echo $mpinfo >> cpu_usage.plot
+		ioStat=`iostat -t | awk '{ $1=""; $5=""; $6=""; print $0;}'`
+		#Trims the header and locates the specified item in the row of data from iostat
+		ioData="`echo ${ioStat:51:8}` `echo ${ioStat:152:100}`"
+		echo $ioData >> disk_usage.plot
+		sleep $1
+	done	
 }
 
-io="$(iostat -d)"
-mp="$(mpstat)"
+#Creates the bar graph using the retrieved information from iostat and mpstat using the bargraph.pl script
+createGraph()
+{
+	perl bargraph.pl cpu_usage.plot > cpu_usage.eps
+	perl bargraph.pl disk_usage.plot > disk_usage.eps
+}
 
-	echo -n "${mp}" | awk '{print $4}'
-	echo -n ''
-	echo -n "${mp}" | awk '{print $6}'
-	echo -n ''
-	echo -n "${mp}" | awk '{print $13}'
-	echo -n ''
-	echo -n "${io}" | awk '{print $3}'
-	echo -n ''
-	echo -n "${io}" | awk '{print $4}'
-	echo -n ''
-
-
-
-
+#Function Calls
+parameters $1 $2
+makeFiles
+statPlot $1 $2
+createGraph	
 
 
